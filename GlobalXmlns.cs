b432a08a7c84bf32dfb5a0f42f02b0c3a27a82cf
LukaszOrzeszek <lo@11x.eu>
2025-11-15 23:06:35 +0100

    Global Assemblies

---
+++ GlobalXmlns.cs
@@ -0,0 +1,6 @@
+﻿[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PackMeUp.Converters")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PackMeUp.ViewModels")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PackMeUp.Models")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MPowerKit.Lottie", AssemblyName = "MPowerKit.Lottie")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CommunityToolkit.Maui.Behaviors", AssemblyName = "CommunityToolkit.Maui")]
